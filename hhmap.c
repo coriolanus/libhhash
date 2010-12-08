8354d7b7190b7dd019626f6d1ebaea2ef6f4d99f
Jani Lahtinen <jani.lahtinen8@gmail.com>
2010-12-08 11:47:30 +0200

    All source files in the base directory

---
+++ hhmap.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "hhash.h"
+#include "hhset.h"
+#include "hhmap.h"
+
+#define HALF \
+	(8*sizeof(ushort))
+
+static uint
+id(uint x)
+{
+	return (ushort)x;
+}
+
+static int
+neq(uint x, uint y)
+{
+	return (ushort)x != (ushort)y;
+}
+
+HHSet *
+hhmapnew(uint n, void *hash, void *cmp)
+{
+	HHSet *S = malloc(sizeof(HHSet));
+	S->T = hhashnew(n);
+	S->hash = (hash != NULL) ? hash : id;
+	S->cmp = (cmp != NULL) ? cmp : neq;
+	return S;
+}
+
+int
+hhmapput(HHSet *S, ushort k, ushort v)
+{
+	if (v == 0)
+		return 0;
+	uint x = (((uint)v)<<HALF)|k;
+	return hhsetput(S,x);
+}
+
+ushort
+hhmapget(HHSet *S, ushort k)
+{
+	return (ushort)(hhsetget(S,k)>>HALF);
+}
+
+ushort
+hhmapdel(HHSet *S, ushort k)
+{
+	return (ushort)(hhsetdel(S,k)>>HALF);
+}
---
+++ hhmap.h
@@ -0,0 +1,11 @@
+#ifndef LIBHHMAP_H
+#define LIBHHMAP_H
+
+typedef unsigned short ushort;
+
+HHSet *hhmapnew(uint, void*, void*);
+int hhmapput(HHSet*, ushort, ushort);
+ushort hhmapget(HHSet*, ushort);
+ushort hhmapdel(HHSet*, ushort);
+
+#endif
